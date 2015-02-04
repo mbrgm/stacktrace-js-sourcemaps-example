6b267cfba76b79becfa1956e3c146de6195240cd
Marius Bergmann <marius@yeai.de>
2015-02-04 21:49:48 +0100

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+// Webpack
+require('../index.html')
+
+var StackTrace = require('stacktrace-js')
+
+try {
+    require('./app')
+} catch (e) {
+    StackTrace.fromError(e).then(function(result) {
+        console.log(result)
+    }, function(err) {
+        console.log(err)
+    })
+}
